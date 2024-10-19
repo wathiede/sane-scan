8e6cabafb1683c938c70ddbcd1b2043d48a5b8f8
Bill Thiede <git@xinu.tv>
2024-10-19 00:21:15 +0000

    Only have one example

--- examples/stream3.rs
+++
@@ -1,9 +0,0 @@
-use sane_scan;
-
-fn main() -> anyhow::Result<()> {
-    let sane = sane_scan::Sane::init_1_0()?;
-    for dev in sane.get_devices()? {
-        println!("{dev:?}");
-    }
-    Ok(())
-}
